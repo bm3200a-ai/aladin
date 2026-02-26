54faa89a89e3d5208618312ba0bc8caa9a892ecc
bm3200a-ai <bm3200a@gmail.com>
2026-02-26 16:42:46 +0100

    Add server.js to project structure
    Added server.js file to project structure.
    
    Signed-off-by: bm3200a-ai <bm3200a@gmail.com>

---
+++ sever.js
@@ -0,0 +1,6 @@
+aladdin-web/        <-- dein Hauptordner
+│
+├── server.js       <-- hier kommt die neue Datei hin
+├── package.json    <-- die Datei, die du zuvor erstellt hast
+├── public/         <-- dein Ordner für Frontend-Dateien
+    └── index.html
